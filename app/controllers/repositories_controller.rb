3a540c1cd459dd3c821af162a5693735767e0689
Amanda Wehrman <arwehrman@gmail.com>
2018-02-13 20:49:40 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,10 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    @stubbed_request = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      #req.params['client_id'] = client_id
+      #req.params['client_secret'] = client_secret
+      req.params['query'] = 'tetris'
+    end
   end
 end
